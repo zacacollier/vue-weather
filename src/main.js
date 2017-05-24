730e8fadbe151cfbb784a470e34665b174c1e48e
Zac Collier <zacacollier@gmail.com>
2017-05-24 15:02:13 -0500

    Refactor store to separate file

--- src/main.js
+++ src/main.js
@@ -1,38 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable no-new */
+
 import Vue from 'vue';
-import Vuex from 'vuex';
 import App from './App';
 import router from './router';
+import store from './store';
 
 Vue.config.productionTip = false;
-Vue.use(Vuex);
-Vue.config.devtools = true;
-
-/* eslint-disable no-new */
-const store = new Vuex.Store({
-  state: {
-    search: {
-      value: '',
-    },
-    fetch: {
-      // e.g. Search, WeatherAPI
-      source: '',
-      pending: false,
-      success: false,
-      error: '',
-    },
-  },
-  mutations: {
-    searchValueChange (state, payload) {
-      state.search.value = payload;
-    },
-    handleSearchSubmit (state, payload) {
-      console.log(payload);
-      // state = { ...state, fetch: { ...state.fetch, pending: true, }, };
-    },
-  },
-});
 
 Vue.component('searchbar', {
   name: 'searchbar',
--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,30 @@
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-//
-// Vue.use(Vuex);
-// Vue.config.devtools = true;
-//
-// const store = new Vuex.Store({
-//   state: {
-//     search: {
-//       value: '',
-//     },
-//     fetch: {
-//       // e.g. Search, WeatherAPI
-//       source: '',
-//       pending: false,
-//       success: false,
-//       error: '',
-//     },
-//   },
-//   mutations: {
-//     searchValueChange (state, payload) {
-//       state.search.value = payload;
-//     },
-//   },
-// });
-//
-// export default store;
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+Vue.config.devtools = true;
+
+const store = new Vuex.Store({
+  state: {
+    search: {
+      value: '',
+    },
+    fetch: {
+      // e.g. Search, WeatherAPI
+      source: '',
+      pending: false,
+      success: false,
+      error: '',
+    },
+  },
+  mutations: {
+    searchValueChange (state, payload) {
+      state.search.value = payload;
+    },
+    handleSearchSubmit (state, payload) {
+      console.log(payload);
+      // state = { ...state, fetch: { ...state.fetch, pending: true, }, };
+    },
+  },
+});
+export default store;
