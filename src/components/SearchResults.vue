f54c02b97adc945b55181c70c05c178feb57b360
Zac Collier <zacacollier@gmail.com>
2017-05-25 17:29:16 -0500

    Rendering results in SearchResults component

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="search-results">
+    <h1>{{ results.city.name }}</h1>
+    <h2>{{ results.city.country }}</h2>
   </div>
 </template>
 
@@ -7,7 +9,7 @@
 export default {
   name: 'search-results',
   computed: {
-    get () {
+    results () {
       return this.$store.state.fetch.results;
     },
   },
